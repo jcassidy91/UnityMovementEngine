bf97f7051ef2f75eb854f54fedf477c6088a0e20
Jamie <jamie@mvcodeclub.com>
2018-03-14 00:05:20 -0700

    movement

---
+++ MovementEngine/Assets/cameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public float sensitivity = 15f;
+    public float targetDistance = 2;
+    public GameObject target;
+
+    float pitch = 0;
+    float pitchMin = -40;
+    float pitchMax = 60;
+    float yaw = 0;
+
+    Vector3 smoothingVector;
+    Vector3 targetRotation;
+    Vector3 currentRotation;
+    float smoothTime = 0.12f;
+
+    void LateUpdate () {
+        pitch -= sensitivity * Input.GetAxis("Mouse Y");
+        yaw += sensitivity * Input.GetAxis("Mouse X");
+        pitch = Mathf.Clamp(pitch, pitchMin, pitchMax);
+
+        targetRotation = new Vector3(pitch, yaw, 0);
+        currentRotation = Vector3.SmoothDamp(currentRotation, targetRotation, ref smoothingVector, smoothTime);
+        transform.localEulerAngles = currentRotation;
+
+        transform.position = target.transform.position - targetDistance * transform.forward + Vector3.up * 1f;
+    }
+}
---
+++ MovementEngine/Assets/playerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+    float moveSpeed = 6f;
+    public GameObject cam;
+
+    float smoothTime = 0.06f;
+    float smoothingAngle;
+    float currentRotation;
+
+    CharacterController body;
+    Vector3 velocity;
+    Vector3 smoothingVelocity;
+    float gravity = -12f;
+
+    void Start()
+    {
+        body = GetComponent<CharacterController>();
+    }
+
+    void Update () {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float zInput = Input.GetAxisRaw("Vertical");
+        Vector3 moveInput = new Vector3(xInput, 0, zInput);
+
+        if (moveInput.magnitude > 0)
+        {
+            Move(moveInput.x, moveInput.z);
+        }
+	}
+
+    void Move(float x, float z)
+    {
+        float yAngle = Mathf.Atan2(-z, x) * Mathf.Rad2Deg + cam.transform.localEulerAngles.y;
+        float targetAngle = yAngle;
+        currentRotation = Mathf.SmoothDampAngle(currentRotation, targetAngle, ref smoothingAngle, smoothTime);
+        transform.localEulerAngles = currentRotation * Vector3.up;
+        Vector3 targetVelocity = transform.right * moveSpeed;
+        velocity = Vector3.SmoothDamp(velocity, targetVelocity, ref smoothingVelocity, smoothTime);
+
+        if (!body.isGrounded)
+        {
+            velocity += Vector3.up * gravity;
+        }
+
+        body.Move(velocity * Time.deltaTime);        
+    }
+}
